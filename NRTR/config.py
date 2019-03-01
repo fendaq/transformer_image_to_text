b3aadea752e8e01580c3bc306604023af7f2b25e
Tran Hai Anh Vo <vnbot2@gmail.com>
2019-03-02 02:11:08 +0700

    first commit

---
+++ NRTR/config.py
@@ -0,0 +1,8 @@
+# Constants
+
+# Supported characters
+# First token represents "nothing"
+CHAR_VECTOR = "*0123456789abcdefghijklmnopqrstuvwxyz-'.!?,\""
+
+# Number of classes
+NUM_CLASSES = 64 #len(CHAR_VECTOR) + 1
